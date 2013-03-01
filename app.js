7c6460929c7aafc15f13ae550f8a34ea63726cc1
Adrian Lee <jun.irok@gmail.com>
2013-02-28 22:21:37 -0500

    Added neo4j

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://2be69f6fc:939366037@ec9a7c4ee.hosted.neo4j.org:7355/db/data/');
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
 
 app.get('/', function(req, res) {
   res.send('Hello World');
