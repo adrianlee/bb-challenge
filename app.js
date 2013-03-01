f88496939b326f67b773d9e319b0a1107260f8a2
Adrian Lee <jun.irok@gmail.com>
2013-02-28 22:51:16 -0500

    Able to connect to neo4j db

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ var express = require('express');
 var app = express();
 
 var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://2be69f6fc:939366037@ec9a7c4ee.hosted.neo4j.org:7355/db/data/');
+var db = new neo4j.GraphDatabase('http://2be69f6fc:939366037@ec9a7c4ee.hosted.neo4j.org:7355');
 
 app.use(express.bodyParser());
 
+// Hello World
 app.get('/', function(req, res) {
   res.send({ greeting: 'Hello World' });
 });
 
+// Init
+app.get('/init', function(req, res) {
+  var node = db.createNode({hello: 'world'});
+  node.save(function (err, node) {
+      if (err) {
+          console.err('Error saving new node to database:', err);
+      } else {
+          console.log('Node saved to database with id:', node.id);
+      }
+  });
+  res.send(200);
+});
+
+// Search
 app.get('/search', function(req, res) {
   var payload = [];
 
