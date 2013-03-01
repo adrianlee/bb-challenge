3e8c17f84eb87f16d53f1e1b5465cb74b7f1109e
Adrian Lee <jun.irok@gmail.com>
2013-02-28 22:33:38 -0500

    Define /search endpoint. --can't connect to heroku neo4j

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://2be69f6fc:939366037@ec9a7c4ee.hosted.neo4j.org:7355/db/data/');
 
 app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.send({ greeting: 'Hello World' });
+});
+
+app.get('/search', function(req, res) {
+  var payload = [];
+
+  if (!req.param("to") || !req.param("from")) {
+    return res.send("Invalid Query")
+  }
+
+  console.log(req.param("to"));
+  console.log(req.param("from"));
+
+  payload.push({ to: req.param("to"), from: req.param("from"), duration: 0 });
+
+  res.send(payload);
 });
 
 app.listen(3000);
