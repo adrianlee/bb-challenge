d093075b2b5889dc1eefa69e641982572c0f1b31
Adrian Lee <jun.irok@gmail.com>
2013-03-01 09:01:17 -0500

    Implemented init endpoints to import data to neo4j db

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var neo = require('./lib/neo');
 var app = express();
 
+var redis = require('./lib/redis');
+var async = require('async');
+
 // Express Middleware
 app.use(express.bodyParser());
 
@@ -10,12 +13,18 @@ app.get('/', function (req, res) {
   res.send({ greeting: 'Hello World' });
 });
 
-// Init
-app.get('/init', function (req, res) {
-  neo.createOne(function () {
-    res.send(200);
-  });
+// initialize
+app.get('/init1', function (req, res) {
+  neo.populateDB();
+  res.send(200);
 });
+
+// setRelationships
+app.get('/init2', function (req, res) {
+  neo.setRelationships();
+  res.send(200);
+});
+
 
 // Search
 app.get('/search', function (req, res) {
@@ -25,13 +34,42 @@ app.get('/search', function (req, res) {
     return res.send("Invalid Query")
   }
 
-  console.log(req.param("to"));
-  console.log(req.param("from"));
+  // Get Ids
+  async.parallel({
+    to: function (cb) {
+      redis.getId(req.param("to"), function (r) {
+        cb(null, r);
+      });
+    },
+    from: function (cb) {
+      redis.getId(req.param("from"), function (r) {
+        cb(null, r);
+      });
+    }
+  }, function (err, results) {
+    console.log(results);
 
-  payload.push({ to: req.param("to"), from: req.param("from"), duration: 0 });
+    // retrieve node
+    neo.retrieveById(results.to, function (err, node) {
+      if (err) return console.log(err);
+      console.dir(node.data);
+    });
 
-  res.send(payload);
+    neo.retrieveById(results.from, function (err, node) {
+      if (err) return console.log(err);
+      console.dir(node.data);
+    });
+
+    payload.push({ to: req.param("to"), from: req.param("from"), duration: 0 });
+    res.send(payload);
+  });
+
 });
 
 app.listen(3000);
 console.log('Listening on port 3000');
+
+process.on('uncaughtException', function (err) {
+  console.log("uncaughtException:");
+  console.log(err);
+});
