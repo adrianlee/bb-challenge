7ec20bfaecd286b912606c18b1154e3809657b96
Adrian Lee <jun.irok@gmail.com>
2013-02-28 22:58:14 -0500

    Modularize neo stuff. Added init endpoint for quick purge & fill db

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
 var express = require('express');
+var neo = require('./lib/neo');
 var app = express();
 
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://2be69f6fc:939366037@ec9a7c4ee.hosted.neo4j.org:7355');
-
+// Express Middleware
 app.use(express.bodyParser());
 
 // Hello World
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send({ greeting: 'Hello World' });
 });
 
 // Init
-app.get('/init', function(req, res) {
-  var node = db.createNode({hello: 'world'});
-  node.save(function (err, node) {
-      if (err) {
-          console.err('Error saving new node to database:', err);
-      } else {
-          console.log('Node saved to database with id:', node.id);
-      }
+app.get('/init', function (req, res) {
+  neo.createOne(function () {
+    res.send(200);
   });
-  res.send(200);
 });
 
 // Search
-app.get('/search', function(req, res) {
+app.get('/search', function (req, res) {
   var payload = [];
 
   if (!req.param("to") || !req.param("from")) {
